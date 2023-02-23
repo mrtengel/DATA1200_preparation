2a0f27774e140801018542d4ce230eea14e2ee43
mrtengel <63978037+mrtengel@users.noreply.github.com>
2023-02-23 08:11:26 +0100

    JS txt in tbl 01

--- static/script/basics.js
+++ static/script/basics.js
@@ -1,1 +1,30 @@
-document.write("Hello World");
+let winnerList = [];
+    let winner1 = {
+        name:"Tengel Sandtrø",
+        team:"Alpha"
+    };
+
+    let winner2 = {
+        name:'Klara Sandtrø',
+        team:'Beta'
+    };
+
+    let winner3 = {
+        name:'Aurora Aas',
+        team:'Alpha'
+    };
+
+winnerList.push(winner1);
+winnerList.push(winner2);
+winnerList.push(winner3);
+
+
+let teamList = ['Alpha', 'Beta'];
+    function winners() {
+        document.getElementById("first-place-name").innerHTML = winnerList[0].name;
+            document.getElementById("first-place-team").innerHTML = winnerList[0].team;;
+        document.getElementById("second-place-name").innerHTML = winnerList[1].name;
+            document.getElementById("second-place-team").innerHTML = winnerList[1].team;
+        document.getElementById("third-place-name").innerHTML = winnerList[2].name;
+            document.getElementById("third-place-team").innerHTML = winnerList[2].team;
+    }
